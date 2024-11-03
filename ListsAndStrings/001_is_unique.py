9688b7c082ce97d8fdb98f27875d9be8a900804f
TehreemAkhtar <tehreemakhtar33@gmail.com>
2024-11-03 22:14:32 +0500

    Add Space and Time complexity to the solution

--- ListsAndStrings/001_is_unique.py
+++ ListsAndStrings/001_is_unique.py
@@ -1,12 +1,15 @@
 # Is Unique: Implement an algorithm to determine if a string has all unique characters.
-# What if you cannot use additional data structures?
 
 # Assuming uniqueness is case-insensitive
+# Time Complexity (TC): O(n)
+# Space Complexity (SC): O(n)
 def is_unique_1(string):
     unique_chars = set()
     string = string.lower()
     for char in string:
+        # Average TC O(1): sets use hashing
         if char in unique_chars:
             return False
+        # Average TC O(1): sets use hashing
         unique_chars.add(char)
     return True
