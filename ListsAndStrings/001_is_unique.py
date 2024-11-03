2ec432321af0886d395ade018a097c8350a19564
TehreemAkhtar <tehreemakhtar33@gmail.com>
2024-11-03 22:31:41 +0500

    Add solution #2

--- ListsAndStrings/001_is_unique.py
+++ ListsAndStrings/001_is_unique.py
@@ -13,3 +13,18 @@ def is_unique_1(string):
         # Average TC O(1): sets use hashing
         unique_chars.add(char)
     return True
+
+
+# Assuming uniqueness is case-insensitive
+# Time Complexity (TC): O(n2)
+# Space Complexity (SC): O(n)
+def is_unique_2(string):
+    unique_chars = ''
+    string = string.lower()
+    for char in string:
+        # Average TC O(n): searches linearly
+        if char in unique_chars:
+            return False
+        # Average TC O(n): strings are immutable. Creates a new copy everytime
+        unique_chars += char
+    return True
