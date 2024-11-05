6cc79d828a9e5fbf2e2ada52cc5ed74addd63a82
TehreemAkhtar <tehreemakhtar33@gmail.com>
2024-11-05 23:51:39 +0500

    Adding more solutions

--- ListsAndStrings/001_is_unique.py
+++ ListsAndStrings/001_is_unique.py
@@ -5,6 +5,7 @@
 # Space Complexity (SC): O(n)
 # Uses sets
 def is_unique_1(string):
+    string = string.replace(" ", "")
     unique_chars = set()
     string = string.lower()
     for char in string:
@@ -21,6 +22,7 @@ def is_unique_1(string):
 # Space Complexity (SC): O(n)
 # Uses strings
 def is_unique_2(string):
+    string = string.replace(" ", "")
     unique_chars = ''
     string = string.lower()
     for char in string:
@@ -37,6 +39,7 @@ def is_unique_2(string):
 # Space Complexity (SC): O(1)
 # without additional data structure
 def is_unique_3(string):
+    string = string.replace(" ", "")
     string = string.lower()
     length = len(string)
     for i in range(length):
@@ -44,3 +47,26 @@ def is_unique_3(string):
             if string[i] == string[j]:
                 return False
     return True
+
+
+# Assuming uniqueness is case-insensitive
+# Time Complexity (TC): O(n)
+# Space Complexity (SC): O(n)
+# Uses dicts i.e. classic hashmaps
+def is_unique_4(string):
+    unique_chars = {}
+    string = string.replace(" ", "")
+    string = string.lower()
+    for char in string:
+        if char in unique_chars:
+            return False
+        unique_chars[char] = True
+    return True
+
+
+# Assuming uniqueness is case-insensitive
+# Time Complexity (TC): O(n)
+# Space Complexity (SC): O(n)
+def is_unique_5(string):
+    string = string.replace(" ", "").lower()
+    return len(string) == len(set(string))
