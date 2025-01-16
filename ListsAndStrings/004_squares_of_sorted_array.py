210aa62a860754c0719b3cb6da8e784661628c72
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-01-16 21:34:49 +0400

    Add solution: 004_squares_of_sorted_array.py

---
+++ ListsAndStrings/004_squares_of_sorted_array.py
@@ -0,0 +1,17 @@
+# SOURCE: Leetcode
+# https://leetcode.com/problems/squares-of-a-sorted-array
+
+
+# Solution # 1
+
+# Time Complexity (TC): O(n log n)
+# Space Complexity (SC): O(n) because of sorting algo: Tim Sort
+# Approach: Traverse the entire array -> calculate square -> sort using python built-in function
+def squares_of_sorted_array_1(nums):
+    length = len(nums)
+    for i in range(length):
+        nums[i] = nums[i] * nums[i]
+    # TC of python built-in func is O(N log N)
+    # Uses Timsort, a hybrid sorting algorithm derived from merge sort and insertion sort.
+    nums.sort()
+    return nums
