16ab846541f5ab977ee2320fd6b8943fb029983a
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-01-27 20:12:12 +0500

    Add solution #4 using centre-out strategy

--- ListsAndStrings/004_squares_of_sorted_array.py
+++ ListsAndStrings/004_squares_of_sorted_array.py
@@ -29,7 +29,7 @@ def squares_of_sorted_array_2(nums):
 
 # Solution # 3
 
-# Time Complexity (TC): O(n) because of Two Pointers Solution
+# Time Complexity (TC): O(n) because of Two Pointers Solution : center-in strategy
 # Space Complexity (SC): O(n) because of resultant arr
 # Approach: Use two pointer technique because array is already sorted in non-decreasing order
 # -> initialise one left and right most ptr -> compare both square val -> put the max one in resultant arr
@@ -68,21 +68,23 @@ def squares_of_sorted_array_optimised_3(nums):
 
 # Solution # 4
 
-# Time Complexity (TC): O(n) because of Two Pointers Solution
+# Time Complexity (TC): O(n)  => Two Pointers Sol : center-out strategy
 # Space Complexity (SC): O(n) because of resultant arr
 # Approach: Use two pointer technique because array is already sorted in non-decreasing order
-# -> initialise one left and right most ptr -> compare both square val -> put the max one in resultant arr
-# keep incrementing/decrementing ptrs -> keep appending left to the arr i.e. append in the start
+# -> find the abs min value idx TC (n) -> initialise left ptr = centre - 1, right ptr = centre
+# -> start comparing both and update resultant arr accordingly
 def squares_of_sorted_array_4(nums):
     res = []
-    l, r = 0, len(nums) - 1
-    while l <= r:
-        sq_l = nums[l] * nums[l]
-        sq_r = nums[r] * nums[r]
-        if sq_l > sq_r:
-            res.append(sq_l)
-            l += 1
-        else:
-            res.append(sq_r)
-            r -= 1
-    return res[::-1]
+    # Find the index of the smallest absolute value (closest to 0)
+    min_idx = min(range(len(nums)), key=lambda i: abs(nums[i]))
+    l, r = min_idx - 1, min_idx  # Initialize two pointers around the minimum
+
+    # Merge-like process for center-out traversal
+    while l >= 0 or r < len(nums):
+        if l >= 0 and (r >= len(nums) or abs(nums[l]) <= abs(nums[r])):
+            res.append(nums[l] * nums[l])
+            l -= 1
+        elif r < len(nums):
+            res.append(nums[r] * nums[r])
+            r += 1
+    return res
