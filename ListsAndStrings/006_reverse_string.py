b2aed665c65ff72786d8b38370d7d8469b00201d
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-02-05 19:51:51 +0500

    Add solution #1 for reverse_string

---
+++ ListsAndStrings/006_reverse_string.py
@@ -0,0 +1,15 @@
+# SOURCE: Leetcode
+# https://leetcode.com/problems/reverse-string/
+# Solution: https://www.youtube.com/watch?v=_d0T_2Lk2qA (Neetcode)
+
+
+# Solution # 1
+
+# Time Complexity (TC): O(n): two pointer solution
+# Space Complexity (SC): O(1): in-place swapping
+# Approach: Use two pointers i and j -> swap their positions
+def reverse_string(s):
+    j = len(s) - 1
+    for i in range(len(s) // 2):
+        s[i], s[j - i] = s[j - i], s[i]
+    return s
