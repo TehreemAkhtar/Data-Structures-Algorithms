6bffee0cc157b9cc0881b9e88e589d869542af15
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-02-10 20:52:21 +0500

    Add 008_two_sum.py

---
+++ ListsAndStrings/008_two_sum.py
@@ -0,0 +1,12 @@
+# SOURCE: Leetcode
+# https://leetcode.com/problems/two-sum
+# Solution: https://www.youtube.com/watch?v=KLlXCFG5TnA (Neetcode)
+
+
+# Solution # 1
+
+# Time Complexity (TC): O(n): two pointer solution
+# Space Complexity (SC): O(1): in-place swapping
+# Approach:
+def two_sum(nums):
+    pass
