065a1c5bc76791c32ed4184c8a210538432185a8
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-02-25 21:35:54 +0500

    Add solution for two_sum_1

--- ListsAndStrings/008_two_sum.py
+++ ListsAndStrings/008_two_sum.py
@@ -5,8 +5,13 @@
 
 # Solution # 1
 
-# Time Complexity (TC): O(n): two pointer solution
-# Space Complexity (SC): O(1): in-place swapping
+# Time Complexity (TC): O(n): hashmap (dict) to track elements + indices
+# Space Complexity (SC): O(n): using a hashmap which can store n-1 elements in worst case
 # Approach:
-def two_sum(nums):
-    pass
+def two_sum_1(nums, target):
+    unique_elements = dict()
+    for i in range(len(nums)):
+        element = target - nums[i]
+        if element in unique_elements.keys():
+            return [i, unique_elements[element]]
+        unique_elements[nums[i]] = i
