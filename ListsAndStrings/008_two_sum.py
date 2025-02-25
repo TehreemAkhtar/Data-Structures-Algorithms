628698e8097573e1013a64663e5c260f5c2718be
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-02-25 21:49:21 +0500

    Add documentation for two_sum_1

--- ListsAndStrings/008_two_sum.py
+++ ListsAndStrings/008_two_sum.py
@@ -12,6 +12,9 @@ def two_sum_1(nums, target):
     unique_elements = dict()
     for i in range(len(nums)):
         element = target - nums[i]
+        # dict.keys() return a dynamic view object, not a list. Checking 'element in unique_elements.keys()'
+        # does not iterate through all keys - it directly checks the hashtable.
+        # Membership checks are optimized via hashing.
         if element in unique_elements.keys():
             return [i, unique_elements[element]]
         unique_elements[nums[i]] = i
