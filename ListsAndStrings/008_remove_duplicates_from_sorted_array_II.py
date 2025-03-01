7d7b4309c95e54ad98a9637c9fc53af6815e6953
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-03-01 19:08:43 +0500

    Add approach for remove_duplicates_1

--- ListsAndStrings/008_remove_duplicates_from_sorted_array_II.py
+++ ListsAndStrings/008_remove_duplicates_from_sorted_array_II.py
@@ -7,7 +7,10 @@
 
 # Time Complexity (TC): O(n): two pointer solution
 # Space Complexity (SC): O(1): in-place swapping
-# Approach:
+# Approach: Use two pointers l, r. Use r to count duplicates within the array. Use l to keep
+# track of the output array length. Once r reaches the end of duplicate digits streak, run a loop at max
+# 2 times to copy these elements and increment l ptr. Then move r ptr towards new streak of digits.
+# This way at the end, l ptr will contain the required output with at max 2 duplicates.
 def remove_duplicates_1(nums):
     l, r = 0, 0
     while r < len(nums):
