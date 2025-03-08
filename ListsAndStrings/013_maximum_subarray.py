74fc299a52060443d883afe6f4dac799660836f4
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-03-08 13:32:29 +0500

    Update max_sub_array documentation

--- ListsAndStrings/013_maximum_subarray.py
+++ ListsAndStrings/013_maximum_subarray.py
@@ -3,9 +3,10 @@
 # Solution: https://www.youtube.com/watch?v=5WZl3MMT0Eg (Neetcode)
 
 
-# Time Complexity (TC): O(n): Traverses the entire list and tries to typecast each element to int
-# Space Complexity (SC): O(1): using an additional list to store each value to be compared
-# Drawback: try-except is slower because catching an exception is expensive
+# Time Complexity (TC): O(n): Traverse the array once and adjust the ptrs accordingly.
+# Space Complexity (SC): O(1): Using constant extra memory
+# Approach: Kadane's Algorithm: At every step check if curr_sum < 0 (negative) we will simply
+# reset curr_sum because it is not contributing to finding the max sub array sum.
 def max_sub_array(nums):
     """
     :type strs: List[str]
