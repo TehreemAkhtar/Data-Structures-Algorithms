c06103b991fdc2b101d65b040e73445f35f8f120
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-03-08 13:41:47 +0500

    Add longest_subarray documentation

--- ListsAndStrings/015_longest_subarray_1s_after_deleting_one_element.py
+++ ListsAndStrings/015_longest_subarray_1s_after_deleting_one_element.py
@@ -2,9 +2,11 @@
 # https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/
 
 
-# Time Complexity (TC): O(n): Use sliding window approach and traverse the entire array
+# Time Complexity (TC): O(n): Use kadane algo with slight modification
 # Space Complexity (SC): O(1): constant memory is used
-# Approach:
+# Approach: Maintain two sums prev and curr. Prev_sum will sum of prev sub array of 1 i.e.
+# before encountering a zero and curr_sum will hold current count of ones i.e. once a 0 is
+# encountered. At each step we will keep checking is prev+curr sum is greater than max_sum.
 def longest_subarray(nums, k):
     max_sum = curr_sum = prev_sum = 0
     for num in nums:
