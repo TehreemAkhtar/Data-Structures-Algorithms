b8d4035c5c6aa0fa1518661ce763ecde38b5b0fa
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-03-11 07:54:24 +0500

    Add pivot_index_1 documentation

--- ListsAndStrings/017_find_pivot_index.py
+++ ListsAndStrings/017_find_pivot_index.py
@@ -3,6 +3,10 @@
 # Solution: https://www.youtube.com/watch?v=u89i60lYx8U (Neetcode)
 
 # Solution # 1
+# Time Complexity (TC): O(n): use prefix sum approach
+# Space Complexity (SC): O(n): using a list to store prefix sum
+# Approach: Calculate prefix sum first. In second loop, check if left sum = right sum by
+# excluding last index because the difference will always become equal at that point.
 def pivot_index_1(nums):
     """
     :type nums: List[int]
