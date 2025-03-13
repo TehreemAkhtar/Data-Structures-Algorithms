6ca1b26141d2ef5f7e560c950574b1af48c924c0
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-03-13 23:41:05 +0500

    Add subarray_sum_1

--- ListsAndStrings/018_subarray_sum_equals_k.py
+++ ListsAndStrings/018_subarray_sum_equals_k.py
@@ -2,3 +2,19 @@
 # https://leetcode.com/problems/subarray-sum-equals-k
 # Solution: https://www.youtube.com/watch?v=fFVZt-6sgyo (Neetcode)
 
+
+# Brute force
+def subarray_sum_1(nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: int
+    """
+    pairs = 0
+    for i in range(len(nums)):
+        sum_ = 0
+        for j in range(i, len(nums)):
+            sum_ += nums[j]
+            if sum_ == k:
+                pairs += 1
+    return pairs
