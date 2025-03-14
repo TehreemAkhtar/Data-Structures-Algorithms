2fe138b89ac5d8626326a609f73dcf5a358595a7
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-03-14 23:54:39 +0500

    Add subarray_sum_2

--- ListsAndStrings/018_subarray_sum_equals_k.py
+++ ListsAndStrings/018_subarray_sum_equals_k.py
@@ -18,3 +18,20 @@ def subarray_sum_1(nums, k):
             if sum_ == k:
                 pairs += 1
     return pairs
+
+
+def subarray_sum_2(nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: int
+    """
+    pairs = curr_sum = 0
+    prefix_sum_count = {0: 1}
+
+    for n in nums:
+        curr_sum += n
+        diff = curr_sum - k
+        pairs += prefix_sum_count.get(diff, 0)
+        prefix_sum_count[curr_sum] = 1 + prefix_sum_count.get(curr_sum, 0)
+    return pairs
