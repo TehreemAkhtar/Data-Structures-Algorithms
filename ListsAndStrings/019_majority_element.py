466b9c0eebaac378342f2668c60b05fa5aa1d125
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-03-16 23:03:28 +0500

    Add majorityElement

--- ListsAndStrings/019_majority_element.py
+++ ListsAndStrings/019_majority_element.py
@@ -1,3 +1,17 @@
 # SOURCE: Leetcode
 # https://leetcode.com/problems/majority-element
 # Solution: https://www.youtube.com/watch?v=7pnhv842keE (Neetcode)
+
+
+def majorityElement(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    maj_element = len(nums) // 2
+    count = {}
+    for n in nums:
+        count[n] = count.get(n, 0) + 1
+    for k, v in count.items():
+        if v > maj_element:
+            return k
