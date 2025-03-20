c4f84292a3f128d4e77ca149cbf98c250fe92e9d
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-03-20 14:08:59 +0500

    Update majority_element_1 solution

--- ListsAndStrings/019_majority_element.py
+++ ListsAndStrings/019_majority_element.py
@@ -3,7 +3,12 @@
 # Solution: https://www.youtube.com/watch?v=7pnhv842keE (Neetcode)
 
 
-def majorityElement(self, nums):
+# Solution # 1
+# Time Complexity (TC): O(n2): uses brute-force approach
+# Space Complexity (SC): O(1): using a constant amount of extra space
+# Approach: Uses a brute force approach where we check all possible subarray sums
+# and update pairs count
+def majority_element_1(nums):
     """
     :type nums: List[int]
     :rtype: int
