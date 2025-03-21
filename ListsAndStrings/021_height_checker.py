f370dd2e5fc56c52a453823f4fa7c12132800738
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-03-21 16:29:40 +0500

    Add height_checker_1

--- ListsAndStrings/021_height_checker.py
+++ ListsAndStrings/021_height_checker.py
@@ -1,2 +1,15 @@
 # SOURCE: Leetcode
 # https://leetcode.com/problems/height-checker/
+
+
+# Solution # 1
+# Time Complexity (TC): O(nlogn): Python sort function uses Tim sort - nlogn
+# Space Complexity (SC): O(n): uses an extra array to keep the sorted list
+# Approach: create a sorted version of input and then start comparing elements in each iteration
+def height_checker_1(heights):
+    """
+    :type heights: List[int]
+    :rtype: int
+    """
+    expected = sorted(heights)
+    return sum(1 for i in range(len(heights)) if heights[i] != expected[i])
