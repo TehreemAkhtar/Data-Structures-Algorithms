3e27383cb526afd561ddcedae2deec5592a5be7e
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-03-25 13:29:51 +0500

    Refactor file name

--- list_and_strings/022_sort_colors.py
+++ list_and_strings/022_sort_colors.py
@@ -1,6 +1,7 @@
 # SOURCE: Leetcode
 # https://leetcode.com/problems/sort-colors/
 # Solution: https://www.youtube.com/watch?v=4xbWSRZHqac (Neetcode)
+from heapq import heappop
 
 
 # Solution # 1
@@ -32,6 +33,7 @@ def sort_colors(nums):
             r -= 1
         else:
             i += 1
+            heappop()
 
 # Approach 2 can be implemented using counting sort but that would require two passes
 # and the solution requires one pass only
