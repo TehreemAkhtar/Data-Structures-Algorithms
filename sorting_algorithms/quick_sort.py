a12b76cd2458e28863d28a7a7e3c6b21ac06fc8e
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-04-12 21:16:57 +0500

    Add 3-way partition version of quick sort

--- sorting_algorithms/quick_sort.py
+++ sorting_algorithms/quick_sort.py
@@ -1,5 +1,6 @@
 import random
 
+
 # References
 # 1. https://www.youtube.com/watch?v=COk73cpQbFQ (Actual algo)
 # 2. https://www.youtube.com/watch?v=3Bbm3Prd5Fo (Analysis)
@@ -59,6 +60,38 @@ def random_partition(array, start, end):
     return partition(array, start, end)
 
 
-nums = [7, 2, 1, 6, 8, 5, 3, 4]
-quick_sort(nums, 0, 7)
+# Ignores duplicates and uses 3-way partitioning
+def quick_sort_3way(array, start, end):
+    if start < end:
+        lt, gt = random_partition_3way(array, start, end)
+        quick_sort(array, start, lt - 1)
+        quick_sort(array, gt + 1, end)
+
+
+def random_partition_3way(array, start, end):
+    pivot_index = random.randint(start, end)
+    array[pivot_index], array[end] = array[end], array[pivot_index]
+    return partition_3way(array, start, end)
+
+
+def partition_3way(array, start, end):
+    pivot = array[end]
+    i = lt = start
+    gt = end
+
+    while i <= gt:
+        if array[i] < pivot:
+            array[i], array[lt] = array[lt], array[i]
+            lt += 1
+            i += 1
+        elif array[i] > pivot:
+            array[i], array[gt] = array[gt], array[i]
+            gt -= 1
+        else:
+            i += 1
+    return lt, gt
+
+
+nums = [3, 2, 3, 1, 2, 4, 5, 5, 6]
+quick_sort(nums, 0, 8)
 print(nums)
