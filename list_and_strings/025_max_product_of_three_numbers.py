1176c6edf08614302ec3522f3b053dbfd3e6c8fa
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-04-14 22:30:45 +0500

    Add maximum_product_2

--- list_and_strings/025_max_product_of_three_numbers.py
+++ list_and_strings/025_max_product_of_three_numbers.py
@@ -18,3 +18,31 @@ def maximum_product_1(nums):
     p1 = nums[0] * nums[1] * nums[2]
     p2 = nums[-1] * nums[-2] * nums[0]
     return p1 if p1 >= p2 else p2
+
+
+# Solution # 2
+# Time Complexity (TC): O(n): Just one loop
+# Space Complexity (SC): O(1): No extra memory
+# Approach: Iterate through the array once and compute max1, max2, max3, min1 and min2.
+def maximum_product_2(nums):
+    max1 = max2 = max3 = -float('inf')
+    min1 = min2 = float('inf')
+
+    for num in nums:
+        if num > max1:
+            max3 = max2
+            max2 = max1
+            max1 = num
+        elif num > max2:
+            max3 = max2
+            max2 = num
+        elif num > max3:
+            max3 = num
+        if num < min1:
+            min2 = min1
+            min1 = num
+        elif num < min2:
+            min2 = num
+    p1 = max1 * max2 * max3
+    p2 = max1 * min1 * min2
+    return max(p1, p2)
