d5635a1d439e5683b872ea8772a36f03594d5141
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-04-20 12:31:01 +0500

    Update max_product documentation

--- list_and_strings/026_max_product_subarray.py
+++ list_and_strings/026_max_product_subarray.py
@@ -9,6 +9,14 @@
 # Approach: Not actually Kadane algo but a modification of Kadane. Keep track of min and max product
 # in each iteration because we have -ve values and so two -ve's can form a +v2. Keep updating res with the
 # max value and return it in the end.
+# Further explanation:
+# Same as kadane’s algorithm for maximum subarray, where we keep track of max subarray ending at ith index.
+# For this problem since two negatives multiplied together give a positive,
+# we need to keep track of the min subarray ending at ith index AND max subarray ending at ith index.
+# Then for i+1 position, if it’s a negative value, then the max product subarray ending at i+1 position is
+# that value * min subarray ending at ith position (if it’s negative value)
+# Dealing with 0s as elements, the zero resets our min and max subarray ending at 0 element’s index.
+# So both min and max become 1 after encountering 0
 def max_product(nums):
     """
     :type nums: List[int]
