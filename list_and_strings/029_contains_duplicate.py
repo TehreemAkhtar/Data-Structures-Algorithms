92d8f4d6d8fc70ba20ebfa5e6d491e7a8bed1473
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-04-27 13:25:48 +0500

    Add 029_contains_duplicate.py

---
+++ list_and_strings/029_contains_duplicate.py
@@ -0,0 +1,33 @@
+# SOURCE: Leetcode
+# https://leetcode.com/problems/contains-duplicate
+
+
+# Solution # 1
+# Time Complexity (TC): O(n): as set traverses the whole array
+# Space Complexity (SC): O(n): set is using additional memory
+# Approach: As set contains unique elements, so we can compare len of both
+# Drawbacks of this solution: this is not the best solution, suppose the duplicate numbers are in the init of
+# the array and the array have an extreme large length, so you will have to create a extreme large set using
+# more memory and time
+def contains_duplicate_1(nums):
+    """
+    :type nums: List[int]
+    :rtype: bool
+    """
+    return len(nums) != len(set(nums))
+
+
+# Solution # 2
+# Time Complexity (TC): O(n log n): sort + traversing the array
+# Space Complexity (SC): O(1): no extra memory
+def contains_duplicate_2(nums):
+    """
+    :type nums: List[int]
+    :rtype: bool
+    """
+    nums.sort()
+    return any(nums[i] == nums[i + 1] for i in range(len(nums) - 1))
+# First, nums[i] == nums[i + 1] for i in range(len(nums) - 1)
+# ➔ This creates a generator that yields True if any two adjacent elements are equal.
+# Then, any(...)
+# ➔ any() will return True if at least one True appears in the generator.
