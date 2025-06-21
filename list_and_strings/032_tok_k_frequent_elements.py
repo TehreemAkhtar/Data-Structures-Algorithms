d9abd98d3ada12138f313ce5d9d234f8e55fe23f
TehreemAkhtar <tehreemakhtar33@gmail.com>
2025-06-21 17:10:46 +0500

    Add top_k_frequent_2

--- list_and_strings/032_tok_k_frequent_elements.py
+++ list_and_strings/032_tok_k_frequent_elements.py
@@ -15,3 +15,26 @@ def top_k_frequent_1(nums, k):
 
     sorted_count = sorted(count.items(), key=lambda x: x[1], reverse=True)
     return [item[0] for item in sorted_count[:k]]
+
+
+# Time Complexity (TC): O(n)
+# Space Complexity (SC): O(n)
+# Approach: Bucket sort -> items in buckets indexed by frequency → scan buckets in reverse → pick top K.
+# Each index in freq represents the frequency and each element at freq[i] contains a bucket of numbers
+# having that frequency.
+def top_k_frequent_2(nums, k):
+    count = {}
+    # creating a list of buckets:
+    freq = [[] for _ in range(len(nums) + 1)]
+
+    for num in nums:
+        count[num] = 1 + count.get(num, 0)
+    for num, cnt in count.items():
+        freq[cnt].append(num)
+
+    res = []
+    for i in range(len(freq) - 1, 0, -1):
+        for num in freq[i]:
+            res.append(num)
+            if len(res) == k:
+                return res
